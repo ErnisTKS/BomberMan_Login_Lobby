f58ee2a80ed80c1f9a578e24cd5a5f5e1f84a10c
Famy9 <ernis701@gmail.com>
2018-02-22 16:34:17 +0200

    siusti i serva ir atgal (NEVEIKIA)

--- src/sample/Controller/LobbyController.java
+++ src/sample/Controller/LobbyController.java
@@ -2,16 +2,26 @@ package sample.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 
-import java.io.IOException;
+import java.awt.*;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LobbyController implements Initializable {
+
+    public Button joinGame;
+    public TextField msgField;
+    public TextArea msgArea;
+    public ListView playerList;
+    public ListView gameRooms;
+
+
     @FXML
     private AnchorPane rootPane;
 
@@ -21,4 +31,19 @@ public class LobbyController implements Initializable {
     }
 
 
+    public void onJoinGame(ActionEvent event) {
+    }
+
+    public void onExit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    public void onSendMessage(ActionEvent event) {
+        PrintStream output = new PrintStream(client.getOutputStream());
+        while (sc.hasNextLine()) {
+            output.println(_nickname + ": " + sc.nextLine());
+        }
+
+        output.close();
+    }
 }
--- src/sample/Controller/LoginController.java
+++ src/sample/Controller/LoginController.java
@@ -11,25 +11,36 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class LoginController implements Initializable {
 
     public Label nameError;
     public TextField nickName;
 
+    public String ivestasVardas;
+
+    private String _host;
+    private int _port;
+    private String _nickname;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
 
     public void onPressConnect(ActionEvent event) throws IOException {
-        String ivestasVardas = nickName.getText();
+        ivestasVardas = nickName.getText();
         if (!ivestasVardas.equals("")) {
-
-
-
+            _host = "localhost";
+            _port = 12345;
+            run();
             Parent lobby = FXMLLoader.load(getClass().getResource("../LobbyScreen.fxml"));
             Scene scene = new Scene(lobby);
 
@@ -43,4 +54,46 @@ public class LoginController implements Initializable {
         }
 
     }
+
+    public void run() throws UnknownHostException, IOException {
+        // connect client to server
+        Socket client = new Socket(_host, _port);
+        System.out.println("Client successfully connected to server!");
+
+        // create a new thread for server messages handling
+       // new Thread(new ReceivedMessagesHandler(client.getInputStream())).start();
+
+        // ask for a nickname
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a nickname: ");
+        _nickname = ivestasVardas;
+
+        // read messages from keyboard and send to server
+
+        sc.close();
+        client.close();
+    }
+
+    public void onExit(ActionEvent event) {
+        System.exit(0);
+    }
+
+//    class ReceivedMessagesHandler implements Runnable {
+//
+//        private InputStream server;
+//
+//        public ReceivedMessagesHandler(InputStream server) {
+//            this.server = server;
+//        }
+//
+//        @Override
+//        public void run() {
+//            // receive server messages and print out to screen
+//            Scanner s = new Scanner(server);
+//            while (s.hasNextLine()) {
+//                System.out.println(s.nextLine());
+//            }
+//            s.close();
+//        }
+//    }
 }
